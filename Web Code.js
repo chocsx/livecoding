e35972b2528864e43c291cc30305be7d7465b5fc
Ronan Hancock <ronanhanky712@gmail.com>
2015-06-07 18:45:22 +0100

    Create Web Code.js

---
+++ Web Code.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+  $("button.nav-button").click(function() {
+    $("nav.menu-nav").slideToggle(2000)
+    $("option.asdef-options").delay(3000).fadeIn('slow')
+  })
+});
+
+  $(document).ready(function() {
+    $("button.button-to-open-nav-bar").click(function() {
+      $(this).slideUp('slow')
+      $("div.nav-bar-section").slideToggle('slow')
+        $("div.logo-section").slideUp('slow')
+          $("article.main-paragraph").slideUp('slow')
+            $("article.poop-with-it").slideUp('slow')
+              $('input.leave-comment').slideUp('slow')
+                $('button.submit-button-class').slideUp('slow')
+                  $('article.eds-article').slideUp('slow')
+                    $('button.label-button').slideUp('slow')
+                      $('form.develope-form').slideUp('slow')
+    })
+  })
+    $(document).ready(function() {
+      $("div.nav-bar-section").click(function() {
+        $(this).slideUp('slow')
+          $("button.button-to-open-nav-bar").slideDown('slow')
+            $("div.logo-section").slideDown('slow')
+              $('article.main-paragraph').slideDown('slow')
+                $("article.poop-with-it").slideDown('slow')
+                  $("input.leave-comment").slideDown('slow')
+                    $('button.submit-button-class').slideDown('slow')
+                      $('article.eds-article').slideDown('slow')
+      })
+    })
+      $(document).ready(function() {
+        $("span.glyphicon-globe").click(function() {
+          $("article.para-expl").slideToggle('slow')
+        })
+      })
+        $(document).ready(function() {
+              $("span.glyphicon-pencil").click(function() {
+                $("article.esdf-enjoy").slideToggle('slow')
+              })
+        })
+          $(document).ready(function() {
+            $("span.glyphicon-user").click(function() {
+              $("article.asdf-enjoy").slideToggle('slow')
+            })
+          })
+            $(document).ready(function() {
+              $("button.button-to-open-nav-bar").mouseenter(function() {
+                $(this).css(opacity, 1)
+                  $(this).mouseleave(function() {
+                    $(this).css(opacity, 0.6)
+                  })
+              })
+            })
+              $(document).ready(function() {
+                $(window).load(function() {
+                  $('div.logo-section').delay(1000).fadeIn('slow')
+                    $('article.poop-with-it').delay(1000).fadeIn('slow')
+                      $('input.leave-comment').delay(1000).fadeIn('slow')
+                        $('button.submit-button-class').delay(1000).fadeIn('slow')
+                          $('article.eds-article').delay(1000).fadeIn('slow')
+                            $('button.button-to-open-nav-bar').delay(1000).slideDown(2500)
+                })
+              })
+              $(document).ready(function() {
+                $(window).load(function() {
+                  $("article.main-paragraph").delay(1000).fadeIn('slow')
+                })
+              })
+              $(document).ready(function() {
+                $("div.panel").click(function() {
+                  $("div.advertisment").slideToggle('slow')
+                })
+              });
